05be787991eeba838842a3a3f946ad910163e699
FelmArch <ferrlinda.dl@gmail.com>
2026-04-18 11:26:53 +0100

    fix: declare uma funcao que receba um número e retorne esse número + 7

---
+++ javascript/logica.js
@@ -0,0 +1,22 @@
+
+
+
+// Teste um
+
+console.log('Bem vindo de Volta!');
+
+let nome = 'Felmarch';
+console.log(`Seja bem Vindo ${nome}`);
+
+
+
+
+// Iremos aqui passar os exercicios de Nocoes basicas de funcionamento(Function)
+
+// Escreva uma função chamada `f` add7que receba um número e retorne esse número + 7. add7(10)deve retornar17
+
+
+function adicionar7(num) {
+    return num + 7;
+}
+console.log(adicionar7(10));
