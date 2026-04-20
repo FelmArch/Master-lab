ddee0b59f85df6935c99ba2490e4ae50dd338334
FelmArch <ferrlinda.dl@gmail.com>
2026-04-20 11:09:50 +0100

    feat: resolva o nr 2 do exercicio, multply

--- javascript/logica.js
+++ javascript/logica.js
@@ -13,10 +13,19 @@ console.log(`Seja bem Vindo ${nome}`);
 
 // Iremos aqui passar os exercicios de Nocoes basicas de funcionamento(Function)
 
-// Escreva uma função chamada `f` add7que receba um número e retorne esse número + 7. add7(10)deve retornar17
+// 1 Escreva uma função chamada `f` add7que receba um número e retorne esse número + 7. add7(10)deve retornar17
 
 
 function adicionar7(num) {
     return num + 7;
 }
 console.log(adicionar7(10));
+
+// multiply - recebe dois numeros, retorna o produto
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 2));
+
+// capitalize- manipular letras
