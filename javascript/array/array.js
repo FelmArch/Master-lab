8f0ca4e3612c844f0eb42011ce3616a09d54d907
FelmArch <ferrlinda.dl@gmail.com>
2026-05-07 16:40:02 +0200

    feat: completa exercicios de fundamentos de array

--- javascript/array/array.js
+++ javascript/array/array.js
@@ -54,3 +54,61 @@ function filterRangeInPlace(arr, a, b) {
 let arr = [5, 3, 8, 1];
 filterRangeInPlace(arr, 1, 4);
 console.log(arr); // [3, 1]
+
+
+// Ordene em ordem decrescente
+
+let arr = [5, 2, 1, -10, 8];
+
+// Usamos uma função de comparação (a, b)
+// Se quisermos ordem DECRESCENTE (do maior para o menor):
+arr.sort((a, b) => b - a);
+
+alert(arr); // 8, 5, 2, 1, -10
+
+// 5 Copiar e ordenar matriz
+
+function copySorted(arr) {
+  // 1. Fazemos uma cópia usando [...] (o operador spread)
+  // 2. Ordenamos a cópia com .sort()
+  return [...arr].sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+
+alert(sorted); // CSS, HTML, JavaScript
+alert(arr);    // HTML, JavaScript, CSS (inalterado!)
+
+// 6 Embaralhe um array
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    // Escolhe um índice aleatório de 0 a i
+    let j = Math.floor(Math.random() * (i + 1));
+    
+    // Troca os elementos (swap)
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// 7 Filtrar membros únicos da matriz
+
+function unique(arr) {
+  let resultado = [];
+
+  for (let str of arr) {
+    if (!resultado.includes(str)) {
+      resultado.push(str);
+    }
+  }
+
+  return resultado;
+}
+
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(strings) ); // Hare, Krishna, :-O
